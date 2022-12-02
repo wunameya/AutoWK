bb66aff10294a3431797ba6d2b6d1c0e695e79f5
wuname_1880361474123 <wunameya@gmail.com>
2022-12-02 18:58:52 +0800

    rua

--- src/main/java/A/Main.java
+++ src/main/java/A/Main.java
@@ -1,15 +1,11 @@
 package A;
 
-import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.event.InputEvent;
-import java.awt.image.BufferedImage;
-import java.io.File;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class Main {
-
     public static String find(String keyImagePath) {
         ImageFindDemo demo = new ImageFindDemo(keyImagePath);
         if(demo.result){
@@ -18,7 +14,6 @@ public class Main {
         return "";
     }
     public static void main(String[] args) throws Exception{
-        System.out.println(find("C:\\Users\\wuname_1880361474123\\IdeaProjects\\BZWM\\src\\main\\resources\\p\\10.png"));
             System.out.println("如果程序出现问题，请关闭此程序和钉钉并重新启动");
             Thread.sleep(5000);
             System.out.println("钉钉半自动网课辣鸡程序启动，请打开钉钉,并最小化此窗口");
@@ -27,7 +22,7 @@ public class Main {
             Thread.sleep(5000);
             while (true){
                 Thread.sleep(10000);
-                String s=find("C:\\Users\\wuname_1880361474123\\IdeaProjects\\BZWM\\src\\main\\resources\\p\\8.png");
+                String s=find("8.png");
                 if(!s.equals("")) {
                     log("发现开始直播，进入直播间");
                     Mouse(Integer.parseInt(s.split(",")[0]),Integer.parseInt(s.split(",")[1]));
@@ -36,8 +31,8 @@ public class Main {
                     //Mouse(525,371);
                 }
                 while (!s.equals("")){
-                    String s1=find("C:\\Users\\wuname_1880361474123\\IdeaProjects\\BZWM\\src\\main\\resources\\p\\9.png");
-                    String s2=find("C:\\Users\\wuname_1880361474123\\IdeaProjects\\BZWM\\src\\main\\resources\\p\\10.png");
+                    String s1=find("9.png");
+                    String s2=find("10.png");
                     Thread.sleep((20000));
                     //观测直播间是否有签到
                     if(!s1.equals("")){
@@ -47,7 +42,7 @@ public class Main {
                     //观察是否结束
                     if(!s2.equals("")){
                         log("发现直播结束，退出直播");
-                        String s3=find("C:\\Users\\wuname_1880361474123\\IdeaProjects\\BZWM\\src\\main\\resources\\p\\2.png");
+                        String s3=find("2.png");
                         Mouse(Integer.parseInt(s3.split(",")[0]),Integer.parseInt(s3.split(",")[1]));
                         break;
                     }
